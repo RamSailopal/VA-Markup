3b8414858b00d7d4f78f2cca6a315d3d979309ed
Raman Sailopal <thelinuxguys@yahoo.com>
2022-10-06 09:46:03 +0100

    Update scrapy3.sh

--- scrapy3.sh
+++ scrapy3.sh
@@ -10,7 +10,7 @@ echo -e "table, th, td {\nborder: 1px solid black;\n}" >> table.html
 echo -e "</style>\n" >> table.html
 echo -e  "<body>\n" >> table.html
 echo -e "<table>\n" >> table.html
-echo -e "<tr><th>Section</th><th>Package</th><th>Title</th><th>DOCX</th><th>PDF</th></tr>\n" >> table.html
+echo -e "<tr><th>Section</th><th>Package</th><th>Title</th><th>PDF</th><th>DOCX</th></tr>\n" >> table.html
 sed -i "s@start_urls = \['.*'\]@start_urls = ['"$1"']@" scrape3.py
 scrapy runspider scrape3.py >> table.html
 echo -e "</table>\n" >> table.html
